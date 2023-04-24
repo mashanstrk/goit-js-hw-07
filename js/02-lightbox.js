bf58298b4c320c6bf017f7b26843e1cd10c7ce4f
mashanstrk <117375259+mashanstrk@users.noreply.github.com>
2023-04-24 22:15:35 +0300

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+// Створення розмітки галереї
+
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.map(({preview, original, description}) => `<li class="gallery__item">
+<a class="gallery__link" href=${original}>
+   <img class="gallery__image" src=${preview} alt=${description} />
+</a>
+</li>`).join('');
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+
+// Додавання модального вікна
+
+// const lightbox = new SimpleLightbox('.gallery a');
+
+new SimpleLightbox('.gallery a', {
+    captionsData:"alt",
+    captionDelay: 250,
+});
